2daafc089c5daad94b9b2cbe76963ebb69c02c94
Maximilian Walther <maximilian.walther@spryfox.de>
2022-11-15 20:53:56 +0000

    "Brew formula update for homebrew-awspm version v0.0.22"

--- formula/homebrew-awspm.rb
+++ formula/homebrew-awspm.rb
@@ -5,8 +5,8 @@
 class HomebrewAwspm < Formula
   desc "Awspm, the aws profile manager, helps to set up your aws environment for deployi"
   homepage "https://github.com/spryfox-analytics/homebrew-awspm"
-  url "https://github.com/spryfox-analytics/homebrew-awspm/archive/v0.0.21.tar.gz"
-  sha256 "29ac3eb53b118e7c646d01cfcfbf6011707558664b81219cda359ecdc42a2e04"
+  url "https://github.com/spryfox-analytics/homebrew-awspm/archive/v0.0.22.tar.gz"
+  sha256 "3692ba5b65f3b8881761442a4a070aaeb7662ecfea9ffb4fdebbdcc21d7f8772"
   license ""
 
   depends_on "bash" => :build
@@ -14,8 +14,8 @@ class HomebrewAwspm < Formula
   on_macos do
 
     on_arm do
-      url "https://github.com/spryfox-analytics/homebrew-awspm/releases/download/v0.0.21/homebrew-awspm-0.0.21-darwin-arm64.tar.gz"
-      sha256 "3629f28f78de472287f0aab432f59bb3d8cb56dc6b04bff02c5ebc4eb310fe8f"
+      url "https://github.com/spryfox-analytics/homebrew-awspm/releases/download/v0.0.22/homebrew-awspm-0.0.22-darwin-arm64.tar.gz"
+      sha256 "ab09d104318cf3ec0455c157fb93daad915c8033bf1e661778f04f16a3be258b"
     end
   end
 
